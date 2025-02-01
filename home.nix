57443f161b519d8546ce58f6948a8596e3f08c52
Olivia Palmu <git@olivialta.cc>
2025-02-01 17:39:12 +0200

    [Generation 164]

--- home.nix
+++ home.nix
@@ -74,7 +74,6 @@
   #  /etc/profiles/per-user/olivia/etc/profile.d/hm-session-vars.sh
   #
   home.sessionVariables = {
-    KICAD_DOCUMENTS_HOME = "/Users/olivia/Documents/code/kicad";
     # EDITOR = "emacs";
   };
 
