9225e5b0757b54d528f7923b319237b0499c428e
RocketRace <git@olivialta.cc>
2025-02-07 23:33:19 +0200

    [Generation 246]

--- home.nix
+++ home.nix
@@ -172,7 +172,7 @@
       "explorer.confirmDelete" = false;
       "explorer.autoReveal" = false;
       "explorer.confirmDragAndDrop" = false;
-      "terminal.external.osxExec" = "iTerm2";
+      "terminal.external.osxExec" = "${pkgs.iterm2}";
       # annoyances of various degrees
       "editor.accessibilitySupport" = "off";
       "workbench.startupEditor" = "none";
