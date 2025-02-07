b51cae423b90d32e7d28da96b89678d62f23b8f2
RocketRace <git@olivialta.cc>
2025-02-07 23:36:51 +0200

    [Generation 249]

--- home.nix
+++ home.nix
@@ -170,7 +170,7 @@
       "explorer.confirmDelete" = false;
       "explorer.autoReveal" = false;
       "explorer.confirmDragAndDrop" = false;
-      "terminal.external.osxExec" = "${pkgs.iterm2}/bin/iterm2";
+      "terminal.external.osxExec" = "Nix Trampolines/iTerm2.app";
       # annoyances of various degrees
       "editor.accessibilitySupport" = "off";
       "workbench.startupEditor" = "none";
