59373ef8f0210048ba702e8a45d5a0e8019cac56
RocketRace <git@olivialta.cc>
2025-07-31 12:38:00 +0900

    Add some media utilities

--- home-manager/utils.nix
+++ home-manager/utils.nix
@@ -4,4 +4,8 @@
   programs.direnv.enable = true;
   # rg
   programs.ripgrep.enable = true;
+  # youtube stuff
+  programs.yt-dlp.enable = true;
+  # media
+  home.packages = with pkgs; [ ffmpeg ];
 }
